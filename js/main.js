3b85b9959d397a9ced2e56b900ba3086e453a386
Gwendolau <gwendolau@gwendolau.com>
2014-05-10 22:36:59 -0400

    Modification des liens de menu
    Ajout de l’affichage du contenu par défaut et du menu vertical en lien
    avec l’onglet sélectionné.

--- js/main.js
+++ js/main.js
@@ -9,6 +9,10 @@ function changeSection(sectionNumber, clickedElement) {
             $(clickedElementParent).addClass("current");
             $("#contenu div").css("display","none");
             $("#conteneurAccueil").css("display","block");
+            $("#left-menu div").css("display","none");
+            $("#menuaccueil").css("display","block");
+            $("#head div").css("display","none");
+            $("#headaccueil").css("display","block");
             break;
         
         case 1:
@@ -17,6 +21,10 @@ function changeSection(sectionNumber, clickedElement) {
             $(clickedElementParent).addClass("current");
             $("#contenu div").css("display","none");
             $("#conteneurReseau").css("display","block");
+            $("#left-menu div").css("display","none");
+            $("#menureseau").css("display","block");
+            $("#head div").css("display","none");
+            $("#headreseau").css("display","block");
             break;
             
         case 2:
@@ -25,6 +33,58 @@ function changeSection(sectionNumber, clickedElement) {
             $(clickedElementParent).addClass("current");
             $("#contenu div").css("display","none");
             $("#conteneurSysAdmin").css("display","block");
+            $("#left-menu div").css("display","none");
+            $("#menusysadmin").css("display","block");
+            $("#head div").css("display","none");
+            $("#headsysadmin").css("display","block");
+            break;
+            
+        case 3:
+            console.log("Suivis de problèmes");
+            $("li").removeClass("current");
+            $(clickedElementParent).addClass("current");
+            $("#contenu div").css("display","none");
+            $("#conteneurbugtracker").css("display","block");
+            $("#left-menu div").css("display","none");
+            $("#menubugtracker").css("display","block");
+            $("#head div").css("display","none");
+            $("#headbugtracker").css("display","block");
+            break;
+        
+        case 4:
+            console.log("Wiki");
+            $("li").removeClass("current");
+            $(clickedElementParent).addClass("current");
+            $("#contenu div").css("display","none");
+            $("#conteneurwiki").css("display","block");
+            $("#left-menu div").css("display","none");
+            $("#menuwiki").css("display","block");
+            $("#head div").css("display","none");
+            $("#headwiki").css("display","block");
+            break;
+            
+        case 5:
+            console.log("Gestion de projet");
+            $("li").removeClass("current");
+            $(clickedElementParent).addClass("current");
+            $("#contenu div").css("display","none");
+            $("#conteneurprojet").css("display","block");
+            $("#left-menu div").css("display","none");
+            $("#menuprojet").css("display","block");
+            $("#head div").css("display","none");
+            $("#headprojet").css("display","block");
+            break;
+            
+        case 6:
+            console.log("Parametre");
+            $("li").removeClass("current");
+            $(clickedElementParent).addClass("current");
+            $("#contenu div").css("display","none");
+            $("#conteneurparametre").css("display","block");
+            $("#left-menu div").css("display","none");
+            $("#menuparametre").css("display","block");
+            $("#head div").css("display","none");
+            $("#headparametre").css("display","block");
             break;
             
         default:
