d1f48b7e71c310b7a27d728415fae23726921cf9
Garrus <propasc@gmail.com>
2014-05-14 13:56:46 -0400

    Modification sur changement de page
    - Retiré définitions de ID inutiles, dans CSS principal
    - Modification noms fonctions, ID et classes
    - Retiré switch/case pour ajouter fonctionnalité par concaténation du
    nom de la section passé à la fonction changeSection

--- js/main.js
+++ js/main.js
@@ -1,100 +1,37 @@
-function changeSection(sectionNumber, clickedElement) {
+/****************************************************
+*   @function changeSection                         *
+*   @param sectionName - STRING                     *
+*   @param clickedElement - DOM Object              *
+*                                                   *
+*   This function changes the content depending     *
+*   on what section was clicked.                    *
+*****************************************************/
+function changeSection(sectionName, clickedElement) {
     "use strict";
-    var clickedElementParent = $(clickedElement.parentNode);
     
-    switch (sectionNumber) {
-        case 0:
-            console.log("Accueil");
-            $("li").removeClass("current");
-            $(clickedElementParent).addClass("current");
-            $("#contenu div").css("display","none");
-            $("#conteneurAccueil").css("display","block");
-            $("#left-menu div").css("display","none");
-            $("#menuaccueil").css("display","block");
-            $("#aproposaccueil div").css("display","none");
-            $("#aproposaccueil").css("display","block");
-            $("#head div").css("display","none");
-            $("#headaccueil").css("display","block");
-            break;
-        
-        case 1:
-            console.log("Reseau");
-            $("li").removeClass("current");
-            $(clickedElementParent).addClass("current");
-            $("#contenu div").css("display","none");
-            $("#conteneurReseau").css("display","block");
-            $("#left-menu div").css("display","none");
-            $("#menureseau").css("display","block");
-            $("#head div").css("display","none");
-            $("#headreseau").css("display","block");
-            $("#aproposreseau div").css("display","none");
-            $("#aproposreseau").css("display","block");
-            $("#testreseau div").css("display","none");
-            $("#testreseau").css("display","block");
-            break;
-            
-        case 2:
-            console.log("Sys Admin");
-            $("li").removeClass("current");
-            $(clickedElementParent).addClass("current");
-            $("#contenu div").css("display","none");
-            $("#conteneurSysAdmin").css("display","block");
-            $("#left-menu div").css("display","none");
-            $("#menusysadmin").css("display","block");
-            $("#head div").css("display","none");
-            $("#headsysadmin").css("display","block");
-            break;
-            
-        case 3:
-            console.log("Suivis de problèmes");
-            $("li").removeClass("current");
-            $(clickedElementParent).addClass("current");
-            $("#contenu div").css("display","none");
-            $("#conteneurbugtracker").css("display","block");
-            $("#left-menu div").css("display","none");
-            $("#menubugtracker").css("display","block");
-            $("#head div").css("display","none");
-            $("#headbugtracker").css("display","block");
-            break;
-        
-        case 4:
-            console.log("Wiki");
-            $("li").removeClass("current");
-            $(clickedElementParent).addClass("current");
-            $("#contenu div").css("display","none");
-            $("#conteneurwiki").css("display","block");
-            $("#left-menu div").css("display","none");
-            $("#menuwiki").css("display","block");
-            $("#head div").css("display","none");
-            $("#headwiki").css("display","block");
-            break;
-            
-        case 5:
-            console.log("Gestion de projet");
-            $("li").removeClass("current");
-            $(clickedElementParent).addClass("current");
-            $("#contenu div").css("display","none");
-            $("#conteneurprojet").css("display","block");
-            $("#left-menu div").css("display","none");
-            $("#menuprojet").css("display","block");
-            $("#head div").css("display","none");
-            $("#headprojet").css("display","block");
-            break;
-            
-        case 6:
-            console.log("Parametre");
-            $("li").removeClass("current");
-            $(clickedElementParent).addClass("current");
-            $("#contenu div").css("display","none");
-            $("#conteneurparametre").css("display","block");
-            $("#left-menu div").css("display","none");
-            $("#menuparametre").css("display","block");
-            $("#head div").css("display","none");
-            $("#headparametre").css("display","block");
-            break;
-            
-        default:
-            console.log("Pas un choix valide");
-            break;
-    }
+    var clickedElementParent = $(clickedElement.parentNode);
+    var clickedSectionNameElement = $("#head"+sectionName);
+    var clickedLeftMenuElement = $("#leftMenu"+sectionName);
+    var clickedSectionContent = $("#conteneur"+sectionName);
+    
+    //  First, we change the page Header
+    $("#head div").removeClass("headActive animated fadeInRight");
+    clickedSectionNameElement.addClass("headActive animated fadeInRight");
+    
+    //  Then, we change the menu's current element
+    $("#menu li").removeClass("current");
+    clickedElementParent.addClass("current");
+    
+    //  Then the left-menu's content
+    $("#left-menu div").removeClass("leftMenuActive");
+    clickedLeftMenuElement.addClass("leftMenuActive");
+    
+    // And at last, the section's main content
+    $("#contenu div").removeClass("contentActive");
+    clickedSectionContent.addClass("contentActive");
+    
 }
+
+$(document).ready(function () {
+    console.log($("#conteneurAccueil").html())
+});
